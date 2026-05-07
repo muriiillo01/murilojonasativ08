d6e598a2db17adfa0bfdb151c3eb6230390dc85b
muriiillo01 <muriloalves20010@gmail.com>
2026-05-07 14:26:05 +0000

    meu primeiro commit

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
-import { open } from 'sqlite';
-import sqlite3 from 'sqlite3';
-
-async function iniciarBanco() {
-    const db = await open({
-        filename: './banco.db',
-        driver: sqlite3.Database
-    });
-
-    await db.run(`
-        CREATE TABLE IF NOT EXISTS usuarios (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nome TEXT,
-            idade INTEGER
-        )
-    `);
-
-    await db.run(
-        "INSERT INTO usuarios (nome, idade) VALUES (?, ?)",
-        ["Maria", 25]
-    );
-
-    const usuarios = await db.all("SELECT * FROM usuarios");
-    
-    console.log("✅ Execução concluída! Dados no banco:");
-    console.log(usuarios);
-}
-
-iniciarBanco();
+usuarios(
+  id INTEGER PRIMARY KEY,
+  nome TEXT,
+  idade INTEGER
+)await db.run(
+  "INSERT INTO usuarios (nome, idade) VALUES (?, ?)",
+  ["Maria", 25]
+);const usuarios = await db.all("SELECT * FROM usuarios");
+console.log(usuarios);
